7f09a5fdb85fef2db2f1f6ebdc01dfa59a56f124
xxax22 <219770465+xxax22@users.noreply.github.com>
2025-09-13 18:44:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 ﻿
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '932d021a-0517-458e-9c78-57adde240b2b';
 let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
@@ -7195,4 +7195,5 @@ function config_Html(token = "test", proxyhost = "") {
 </html>`;
 
     return html;
+
 }
